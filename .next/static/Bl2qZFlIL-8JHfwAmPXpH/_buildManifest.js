73c59d637d03c63b144fe633f09c21a1d6114396
Siddhant Vinchurkar <siddhantvinchurkar@gmail.com>
2020-04-16 16:35:49 +0530

    Mark 2 Creating Static Version

---
+++ .next/static/Bl2qZFlIL-8JHfwAmPXpH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/privacy":[a]}}("static\u002Fchunks\u002Fbfecee7037e22e5f5be516b7e9ca2f9b307a24dd.a18f3fdc61f8d6f65a1a.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
