4ab9861b1c1569531269387927cddc7f7fe2b6eb
Siddhant Vinchurkar <siddhantvinchurkar@gmail.com>
2020-04-16 16:59:53 +0530

    Mark 2 Complete

---
+++ .next/static/rVDx_GIo_Yl7KIxsua3ki/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/privacy":[a]}}("static\u002Fchunks\u002F37de9a0adbd6414065dfea5fea1580156267a2dd.a18f3fdc61f8d6f65a1a.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
