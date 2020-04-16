d2d3dfd85c143a6b842a2eec11161b3560554c89
Siddhant Vinchurkar <siddhantvinchurkar@gmail.com>
2020-04-16 16:25:22 +0530

    Mark 2 Creating Static Version

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0d245c6c2cb821570475";
+/******/ 	var hotCurrentHash = "f7237789f6ee85461ce3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
